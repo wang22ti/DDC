4bd4fb3dfee7d19ca490850e7cc752000da159a2
wang22ti <wang22ti@163.com>
2025-12-17 16:47:03 +0800

    update script cvs

--- bash_script.sh
+++ bash_script.sh
@@ -3,6 +3,7 @@ python cifar_train_sam.py -a resnet32 --dataset cifar100 --imb_type exp --imb_fa
 python cifar_train_sam.py -a resnet32 --dataset cifar100 --imb_type exp --imb_factor 0.01 --loss_type LDAM --train_rule DRW --gpu 0 --seed 0 --tro 0 --gamma 0 --tau 0 --rho 0.5 0.5  --wd 0.0001 --epochs 400 --randaug 1
 python cifar_train_sam.py -a resnet32 --dataset cifar100 --imb_type exp --imb_factor 0.01 --loss_type VS --train_rule None --gpu 0 --seed 0 --tro 0 --gamma 0.05 --tau 1.0 --rho 0.3 0.3  --wd 0.0002 --epochs 400 --randaug 1
 python cifar_train_sam.py -a resnet32 --dataset cifar100 --imb_type exp --imb_factor 0.01 --loss_type VS --train_rule ADRW-T --gpu 0 --seed 0 --tro 0.25 --gamma 0 --tau 0.75 --rho 0.2 0.4  --wd 0.0005 --epochs 400 --randaug 1 --resume 'log/cifar100_resnet32_VS_None_exp_0.01_0.0002_1/seed_0_sam_[0.2, 0.2]_args_(0.0, 0.05, 1.0)/320_ckpt.pth.tar'
+python cifar_train.py -a resnet32 --dataset cifar100 --imb_type exp --imb_factor 0.01 --loss_type CVS --train_rule ADRW --seed 0 --wd 0.0002 --epochs 200 --randaug 0 --tro 0.15 --gamma 0.15 --tau 0.9 --max_scale 0.2 --warmup_epochs 10 --min_scale 0.05
 
 # CIFAR-100 LT (Imb ratio = 10)
 python cifar_train_sam.py -a resnet32 --dataset cifar100 --imb_type exp --imb_factor 0.1 --loss_type CE --train_rule None --gpu 0 --seed 0 --tro 0 --gamma 0 --tau 0 --rho 0.1 0.1  --wd 0.0001 --epochs 400 --randaug 1
